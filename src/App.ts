228310bc21151b23d288e91ebe8419772d32873d
Sharonpa1 <94108962+Sharonpa1@users.noreply.github.com>
2024-05-26 02:44:07 +0300

    Install package for images handeling

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,6 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-import studentRoute from "./routes/student_route";
 import postRoute from "./routes/post_route";
 import bodyParser from "body-parser";
 import authRoute from "./routes/auth_route";
@@ -16,7 +15,6 @@ const initApp = () => {
     mongoose.connect(process.env.DATABASE_URL).then(() => {
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
-      app.use("/student", studentRoute);
       app.use("/post", postRoute);
       app.use("/auth", authRoute);
       resolve(app);
